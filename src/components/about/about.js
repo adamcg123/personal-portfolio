e7a7ee1df526e8f2dbe0b4f5e5ecaee6ec3e598a
adamcg123 <adam.Gondorchin26@gmail.com>
2022-07-27 20:24:00 -0500

    added to my about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,7 +7,7 @@ function About() {
       <h1>Who am I?</h1>
       <div className="my-2">
         <p>
-          Hi my name is Adam, I am 19 years old and I graduated from a full stack web developing boot camp that was from the University Of Minnesota. I am currently looking for work in this field and I made this page to show some of my work.
+          Hi my name is Adam, I am 19 years old and I graduated from a full stack web developing boot camp that was from the University Of Minnesota. I'm a decently quick learner and I get along well with others and I am looking forward to being taught new things also I'm currently looking for work in this field and I made this page to show some of my work.
         </p>
       </div>
     </section>
